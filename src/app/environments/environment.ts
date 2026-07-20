f301f14821e3b967f6333ef45f9e99cd5a3cd7fc
Michael Sowah <sowahm808@gmail.com>
2026-07-19 21:49:44 -0500

    updated

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:5001/api',
+  apiBaseUrl: 'http://localhost:5001/api/v1',
+  apiWithCredentials: false,
   appInsightsConnectionString: '',
   enableMockApi: false,
 } as const;
