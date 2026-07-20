7bf1852be7786c182c0102e11fbc3a58e59a6183
Michael Sowah <sowahm808@gmail.com>
2026-07-20 09:42:20 -0500

    updated

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,9 @@
+// src/environments/environment.prod.ts
+
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://block0app-back-end.onrender.com/api',
-  apiWithCredentials: true,
+  apiBaseUrl: 'https://block0app-back-end.onrender.com/api/v1',
+  apiWithCredentials: false,
   appInsightsConnectionString: '',
   enableMockApi: false,
 } as const;
