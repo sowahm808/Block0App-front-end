765e7a607ace4752e8ca7a11b0ddc048a56e790a
Michael Sowah <sowahm808@gmail.com>
2026-07-21 13:26:50 -0500

    updated

--- src/app/features/dashboard/routes.ts
+++ src/app/features/dashboard/routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
 import { authGuard } from '../../core/guards/auth.guard';
-import { roleGuard } from '../../core/guards/role.guard';
+import { permissionGuard } from '../../core/guards/permission.guard';
 import { DashboardPage } from './pages/dashboard.page';
+import { Permission } from '../../core/models/roles';
+
 export default [
-  { path: '', component: DashboardPage, canActivate: [authGuard, roleGuard], data: { roles: ['Scholar'] } },
+  {
+    path: '',
+    component: DashboardPage,
+    canActivate: [authGuard, permissionGuard],
+    data: {
+      permissions: ['scholar:access'] satisfies Permission[],
+    },
+  },
 ] satisfies Routes;
