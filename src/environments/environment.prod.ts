50a809329d187e79ab8c9175f8ac988edff88ed7
Michael Sowah <sowahm808@gmail.com>
2026-07-20 22:42:00 -0500

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,6 +8,12 @@ export const environment = {
   enableMockApi: false,
   firebase: {
     apiKey: 'AIzaSyB9647vJIAgOwFrqEEaI347xMLb6FZXTKg',
+    authDomain: "blockowhisper.firebaseapp.com",
+    projectId: "blockowhisper",
+    storageBucket: "blockowhisper.firebasestorage.app",
+  messagingSenderId: "545571294957",
+    appId: "1:545571294957:web:5897167bdc6431268b46e4",
+    measurementId: "G-CY8YBJYPDQ",
     identityToolkitUrl: 'https://identitytoolkit.googleapis.com/v1',
     secureTokenUrl: 'https://securetoken.googleapis.com/v1',
   },
