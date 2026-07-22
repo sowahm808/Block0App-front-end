758012413a351f2db5de555677c1f7fa84ca4dab
sowahm808 <sowahm808@gmail.com>
2026-07-22 02:37:57 -0500

    Fix challenge program route

--- src/app/features/challenge/routes.ts
+++ src/app/features/challenge/routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 import { roleGuard } from '../../core/guards/role.guard';
-export default [{ path: 'today', canActivate: [roleGuard], data: { roles: ['Scholar'], pageCategory: 'scholar', title: "Today's challenge", apiPath: '/challenges/current/today' }, loadComponent: () => import('./today-challenge.page').then(m => m.TodayChallengePage) }] satisfies Routes;
+
+export default [
+  {
+    path: 'program',
+    canActivate: [roleGuard],
+    data: {
+      roles: ['Scholar'],
+      pageCategory: 'scholar',
+      title: '21-day Block Zero program',
+      apiPath: '/challenges/current/program',
+    },
+    loadComponent: () => import('./pages/program-structure.page').then((m) => m.ProgramStructurePage),
+  },
+  {
+    path: 'today',
+    canActivate: [roleGuard],
+    data: {
+      roles: ['Scholar'],
+      pageCategory: 'scholar',
+      title: "Today's challenge",
+      apiPath: '/challenges/current/today',
+    },
+    loadComponent: () => import('./today-challenge.page').then((m) => m.TodayChallengePage),
+  },
+] satisfies Routes;
